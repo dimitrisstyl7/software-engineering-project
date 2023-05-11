582f122200bf847642ff3a7b081c7150a56ce98f
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-05-11 19:53:00 +0300

    Project created.

---
+++ findoctor/src/main/java/com/unipi/findoctor/FindoctorApplication.java
@@ -0,0 +1,13 @@
+package com.unipi.findoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindoctorApplication.class, args);
+	}
+
+}
