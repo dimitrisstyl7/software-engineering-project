0de01f024ca648e89f52b8594a56f427825a8c91
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-05-17 02:23:15 +0300

    Working on html files - layout.html

--- findoctor/src/main/java/com/unipi/findoctor/constants/ControllerConstants.java
+++ findoctor/src/main/java/com/unipi/findoctor/constants/ControllerConstants.java
@@ -4,7 +4,11 @@ public class ControllerConstants {
     /* URL */
     public static final String ROOT_URL = "/";
     public static final String INDEX_URL = "/index";
+    public static final String LOGIN_URL = "/login";
+    public static final String REGISTER_URL = "/register";
 
     /* FILE NAMES */
     public static final String INDEX_FILE = "index";
+    public static final String LOGIN_FILE = "login";
+    public static final String REGISTER_FILE = "register";
 }
--- findoctor/src/main/java/com/unipi/findoctor/controllers/AuthController.java
+++ findoctor/src/main/java/com/unipi/findoctor/controllers/AuthController.java
@@ -1,4 +1,18 @@
 package com.unipi.findoctor.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static com.unipi.findoctor.constants.ControllerConstants.*;
+
+@Controller
 public class AuthController {
+    @GetMapping(LOGIN_URL)
+    public String loginPage() {
+        return LOGIN_FILE;
+    }
+    @GetMapping(REGISTER_URL)
+    public String registerPage() {
+        return REGISTER_FILE;
+    }
 }
