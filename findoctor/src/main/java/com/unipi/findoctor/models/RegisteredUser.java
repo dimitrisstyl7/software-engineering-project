ad055f7a48ed87d2a75c4e727d72122fcd44ea4e
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-05-23 21:53:52 +0300

    RegisteredUser updated.

--- findoctor/src/main/java/com/unipi/findoctor/models/RegisteredUser.java
+++ findoctor/src/main/java/com/unipi/findoctor/models/RegisteredUser.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
