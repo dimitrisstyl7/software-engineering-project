b201bd6fcf129cee667098de400bc5dac07d1de5
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-05-24 00:59:51 +0300

    Some repositories added.

--- findoctor/src/main/java/com/unipi/findoctor/FindoctorApplication.java
+++ findoctor/src/main/java/com/unipi/findoctor/FindoctorApplication.java
@@ -1,16 +1,13 @@
 package com.unipi.findoctor;
 
-import com.unipi.findoctor.models.Doctor;
-import com.unipi.findoctor.models.RegisteredUser;
-import com.unipi.findoctor.models.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FindoctorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FindoctorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FindoctorApplication.class, args);
+    }
 
 }
