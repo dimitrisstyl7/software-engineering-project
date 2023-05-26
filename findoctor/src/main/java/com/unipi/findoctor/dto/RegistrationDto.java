7370e733def9122ace27866ddaa5da533a29230f
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-05-26 20:40:08 +0300

    RegistrationDto updated.

--- findoctor/src/main/java/com/unipi/findoctor/dto/RegistrationDto.java
+++ findoctor/src/main/java/com/unipi/findoctor/dto/RegistrationDto.java
@@ -1,33 +1,37 @@
 package com.unipi.findoctor.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Data
 public class RegistrationDto {
-    private Long id;
     @NotEmpty
     private String name;
+
     @NotEmpty
     private String surname;
-    @NotEmpty
-    private String dateOfBirth;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
     @NotEmpty
     private String personalPhone;
+
     @NotEmpty
     private String password;
+
     @NotEmpty
     private String email;
-    @NotEmpty
-    private String userType;
-    private LocalDateTime registeredOn;
+
+    private Boolean isDoctor;
     private String afm;
     private String amka;
     private String specialization;
     private String businessPhone;
     private String city;
     private String address;
-    private boolean isVerified; // by default false
 }
