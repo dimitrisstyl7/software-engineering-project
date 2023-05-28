b095dfd47f948bfe21a34cc929991b79c95ace2d
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-05-28 14:25:09 +0300

    Models updated.

--- findoctor/src/main/java/com/unipi/findoctor/models/Doctor.java
+++ findoctor/src/main/java/com/unipi/findoctor/models/Doctor.java
@@ -3,6 +3,7 @@ package com.unipi.findoctor.models;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Data
 @Entity
 @Table(name = "doctor_tbl")
 public class Doctor {
