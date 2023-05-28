712058f69413db8af31b9cd5c01e9dbb12ce4ee1
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-05-28 14:24:51 +0300

    AuthController updated.

--- findoctor/src/main/java/com/unipi/findoctor/controllers/AuthController.java
+++ findoctor/src/main/java/com/unipi/findoctor/controllers/AuthController.java
@@ -2,7 +2,11 @@ package com.unipi.findoctor.controllers;
 
 import com.unipi.findoctor.dto.RegistrationDto;
 import com.unipi.findoctor.mappers.UserMapper;
+import com.unipi.findoctor.models.Doctor;
+import com.unipi.findoctor.models.Patient;
 import com.unipi.findoctor.models.User;
+import com.unipi.findoctor.services.DoctorService;
+import com.unipi.findoctor.services.PatientService;
 import com.unipi.findoctor.services.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -19,6 +23,8 @@ import static com.unipi.findoctor.constants.ControllerConstants.*;
 @Controller
 public class AuthController {
     private final UserService userService;
+    private final PatientService patientService;
+    private final DoctorService doctorService;
 
     @GetMapping(LOGIN_URL)
     public String loginPage() {
@@ -32,17 +38,38 @@ public class AuthController {
     }
 
     @PostMapping(REGISTER_URL)
-    public String registerSave(@Valid @ModelAttribute("user") RegistrationDto registrationDto,
-                               BindingResult result, Model model) {
+    public String registerSave(@Valid @ModelAttribute("user") RegistrationDto registrationDto, BindingResult result, Model model) {
         if (result.hasErrors()) {
             model.addAttribute("user", registrationDto);
             return REGISTER_FILE;
         }
 
+        if (registrationDto.getIsDoctor()) { // is doctor
+            Doctor existingDoctor = doctorService.findByAfm(registrationDto.getAfm());
+            if (existingDoctor != null && existingDoctor.getAfm() != null && !existingDoctor.getAfm().isEmpty()) {
+                result.rejectValue("afm", "afm.alreadyexists",
+                        "There is already an account registered with this AFM.");
+                model.addAttribute("user", registrationDto);
+                return REGISTER_FILE;
+            }
+        } else { // is patient
+            Patient existingPatient = patientService.findByAmka(registrationDto.getAmka());
+            if (existingPatient != null && existingPatient.getAmka() != null && !existingPatient.getAmka().isEmpty()) {
+                result.rejectValue("amka", "amka.alreadyexists",
+                        "There is already an account registered with this AMKA.");
+                model.addAttribute("user", registrationDto);
+                return REGISTER_FILE;
+            }
+        }
+
         User existingUser = userService.findByUsername(registrationDto.getUsername());
         if (existingUser != null && existingUser.getUsername() != null && !existingUser.getUsername().isEmpty()) {
-            return "redirect:" + REGISTER_URL + "?fail";
+            result.rejectValue("username", "username.alreadyexists",
+                    "There is already an account registered with this username.");
+            model.addAttribute("user", registrationDto);
+            return REGISTER_FILE;
         }
+
 
         User user = UserMapper.mapToUser(registrationDto);
         userService.saveUser(user);
