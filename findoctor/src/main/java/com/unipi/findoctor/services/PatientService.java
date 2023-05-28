cd8051761aa0aff69ccc41b554c992dcda803657
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-05-28 14:27:30 +0300

    PatientService and UserService updated.

--- findoctor/src/main/java/com/unipi/findoctor/services/PatientService.java
+++ findoctor/src/main/java/com/unipi/findoctor/services/PatientService.java
@@ -5,6 +5,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface PatientService {
-    void addPatient();
-    Patient getPatient(String amka);
+    Patient findByAmka(String amka);
 }
