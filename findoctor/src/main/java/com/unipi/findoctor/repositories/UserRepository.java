29ef3089448b25d6888dc90d20cea8c1a8be69f0
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-05-28 14:27:06 +0300

    PatientRepository and UserRepository updated.

--- findoctor/src/main/java/com/unipi/findoctor/repositories/UserRepository.java
+++ findoctor/src/main/java/com/unipi/findoctor/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.unipi.findoctor.repositories;
 
 import com.unipi.findoctor.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
