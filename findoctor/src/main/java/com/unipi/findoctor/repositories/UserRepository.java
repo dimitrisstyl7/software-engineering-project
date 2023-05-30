b4d3e578d0fb2f3a7166430c7f7bb3ffdd541941
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-05-31 01:39:13 +0300

    UserRepository updated.

--- findoctor/src/main/java/com/unipi/findoctor/repositories/UserRepository.java
+++ findoctor/src/main/java/com/unipi/findoctor/repositories/UserRepository.java
@@ -4,7 +4,9 @@ import com.unipi.findoctor.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
     User findByUsername(String username);
+
+    User findFirstByUsername(String username);
 }
 
