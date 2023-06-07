506b536c58d9b91d95bd05a5e0b63bc45930541a
anthonyrouss <anthony.rouss@proton.me>
2023-06-07 12:17:40 +0300

    Changed the appointment @Id.

--- findoctor/src/main/java/com/unipi/findoctor/models/Appointment.java
+++ findoctor/src/main/java/com/unipi/findoctor/models/Appointment.java
@@ -3,30 +3,34 @@ package com.unipi.findoctor.models;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Data
 @Entity
 @Table(name = "appointment_tbl")
 public class Appointment {
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true)
     private Long id;
 
-    @Id
+
     @ManyToOne
     @JoinColumn(name = "patient_amka", referencedColumnName = "amka")
     private Patient patient;
 
-    @Id
     @ManyToOne
     @JoinColumn(name = "doctor_afm", referencedColumnName = "afm")
     private Doctor doctor;
 
-    @Id
-    private LocalDateTime dateTime;
+    private LocalDate date;
+    private LocalTime time_slot;
 }
