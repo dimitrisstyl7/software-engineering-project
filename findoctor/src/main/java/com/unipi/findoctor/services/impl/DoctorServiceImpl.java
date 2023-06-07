904bf22b1225ad8a2763972a07507fbc46d49e9e
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-06-07 21:31:43 +0300

    DoctorServiceImpl updated.

--- findoctor/src/main/java/com/unipi/findoctor/services/impl/DoctorServiceImpl.java
+++ findoctor/src/main/java/com/unipi/findoctor/services/impl/DoctorServiceImpl.java
@@ -2,16 +2,24 @@ package com.unipi.findoctor.services.impl;
 
 import com.unipi.findoctor.models.Doctor;
 import com.unipi.findoctor.repositories.DoctorRepository;
+import com.unipi.findoctor.security.SecurityConfig;
 import com.unipi.findoctor.services.DoctorService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
+@Service
 public class DoctorServiceImpl implements DoctorService {
     private final DoctorRepository doctorRepository;
 
     @Override
     public Doctor findByAfm(String afm) {
         return doctorRepository.findByAfm(afm);
+    }
+
+    @Override
+    public void saveDoctor(Doctor doctor) {
+        doctor.getUser().setPassword(SecurityConfig.passwordEncoder().encode(doctor.getUser().getPassword()));
+        doctorRepository.save(doctor);
     }
 }
