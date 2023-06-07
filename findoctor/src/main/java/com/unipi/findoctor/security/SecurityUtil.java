cdc912c8b5ff1ae760fe3e9aaf3e4f2c28be0afb
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-06-07 19:57:52 +0300

    SecurityUtil.java updated.

--- findoctor/src/main/java/com/unipi/findoctor/security/SecurityUtil.java
+++ findoctor/src/main/java/com/unipi/findoctor/security/SecurityUtil.java
@@ -1,14 +1,18 @@
 package com.unipi.findoctor.security;
 
+import com.unipi.findoctor.dto.UserDto;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SecurityUtil {
-    public static String getSessionUser() {
+    public static UserDto getSessionUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
-            return authentication.getName();
+            return new UserDto(
+                    authentication.getName(),
+                    authentication.getAuthorities().toArray()[0].toString()
+            );
         }
         return null;
     }
