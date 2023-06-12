b58be4991106651edeaf2b10d7754b0ef3314f21
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-06-12 21:12:51 +0300

    AdminService.java updated.

--- findoctor/src/main/java/com/unipi/findoctor/services/AdminService.java
+++ findoctor/src/main/java/com/unipi/findoctor/services/AdminService.java
@@ -1,9 +1,11 @@
 package com.unipi.findoctor.services;
 
 import com.unipi.findoctor.dto.DoctorDto;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface AdminService {
     public List<DoctorDto> findAllDoctors();
 }
