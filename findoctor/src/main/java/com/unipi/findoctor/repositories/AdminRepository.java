5f873693677d9b1374748731e02413132426e6e2
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-06-12 21:28:50 +0300

    AdminRepository.java updated.

--- findoctor/src/main/java/com/unipi/findoctor/repositories/AdminRepository.java
+++ findoctor/src/main/java/com/unipi/findoctor/repositories/AdminRepository.java
@@ -1,11 +1,10 @@
 package com.unipi.findoctor.repositories;
 
 import com.unipi.findoctor.models.Admin;
-import com.unipi.findoctor.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminRepository extends JpaRepository<Admin, User> {
+public interface AdminRepository extends JpaRepository<Admin, Long> {
 
 }
