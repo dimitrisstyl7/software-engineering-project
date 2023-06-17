7355506f1a7cee556c586b54b77e0e5c853364dd
anthonyrouss <anthony.rouss@proton.me>
2023-06-17 15:25:33 +0300

    save appointment functionality.

--- findoctor/src/main/java/com/unipi/findoctor/services/AppointmentService.java
+++ findoctor/src/main/java/com/unipi/findoctor/services/AppointmentService.java
@@ -1,5 +1,6 @@
 package com.unipi.findoctor.services;
 
+import com.unipi.findoctor.dto.AppointmentDto;
 import com.unipi.findoctor.models.Appointment;
 import com.unipi.findoctor.models.Doctor;
 import org.springframework.stereotype.Service;
@@ -12,4 +13,6 @@ import java.util.Map;
 @Service
 public interface AppointmentService {
     Map<String, Boolean> getDoctorAvailableTimeSlots(String username, LocalDate date);
+
+    AppointmentDto saveAppointment(AppointmentDto appointment);
 }
