0c680ed36b9be7926627e80a3371919c384216f9
anthonyrouss <anthony.rouss@proton.me>
2023-06-17 15:25:22 +0300

    save appointment functionality.

--- findoctor/src/main/java/com/unipi/findoctor/repositories/AppointmentRepository.java
+++ findoctor/src/main/java/com/unipi/findoctor/repositories/AppointmentRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
     List<Appointment> findAppointmentsByDoctor_User_UsernameAndDate(String username, LocalDate date);
+
+    Appointment save(Appointment appointment);
 }
