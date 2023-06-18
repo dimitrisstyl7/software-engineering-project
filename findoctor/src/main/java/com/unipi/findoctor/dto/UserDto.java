70b0f12a8fe501580643ee207fffc31d953cf709
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-06-18 14:12:21 +0300

    UserDto.java updated.

--- findoctor/src/main/java/com/unipi/findoctor/dto/UserDto.java
+++ findoctor/src/main/java/com/unipi/findoctor/dto/UserDto.java
@@ -2,9 +2,11 @@ package com.unipi.findoctor.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
     private String username;
     private String userType;
