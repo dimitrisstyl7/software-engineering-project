abd1ac311d92e043274d7c14bdbf0ee8abf2bc6b
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-06-29 14:37:22 +0300

    DoctorController.java updated.

--- findoctor/src/main/java/com/unipi/findoctor/controllers/DoctorController.java
+++ findoctor/src/main/java/com/unipi/findoctor/controllers/DoctorController.java
@@ -1,17 +1,19 @@
 package com.unipi.findoctor.controllers;
 
 import com.unipi.findoctor.dto.DoctorDto;
-import com.unipi.findoctor.dto.UserDto;
 import com.unipi.findoctor.mappers.DoctorMapper;
 import com.unipi.findoctor.models.Doctor;
 import com.unipi.findoctor.repositories.DoctorRepository;
 import com.unipi.findoctor.security.SecurityUtil;
+import com.unipi.findoctor.services.DoctorService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.server.ResponseStatusException;
 
 import static com.unipi.findoctor.constants.ControllerConstants.*;
@@ -22,11 +24,10 @@ public class DoctorController {
     private final SecurityUtil securityUtil;
     private final DoctorRepository doctorRepository;
     private final DoctorMapper doctorMapper;
+    private final DoctorService doctorService;
 
     @GetMapping({DOCTOR_ROOT_URL, DOCTOR_INDEX_URL_1, DOCTOR_INDEX_URL_2})
     public String doctorIndexPage() {
-        UserDto Userdto = securityUtil.getSessionUser();
-        Userdto.getUsername();
         return DOCTOR_INDEX_FILE;
     }
 
@@ -37,7 +38,7 @@ public class DoctorController {
 
     @GetMapping(DOCTOR_BOOKINGS_URL)
     public String doctorBookingsPage(Model model) {
-        model.addAttribute("doctorUsername",securityUtil.getSessionUser().getUsername());
+        model.addAttribute("doctorUsername", securityUtil.getSessionUser().getUsername());
         return DOCTOR_BOOKINGS_FILE;
     }
 
@@ -50,13 +51,20 @@ public class DoctorController {
     public String doctorDoctorProfilePage(Model model) {
         String username = securityUtil.getSessionUser().getUsername();
         Doctor doctor = doctorRepository.findByUser_username(username);
-        if (doctor == null){
-           throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        if (doctor == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         DoctorDto doctorDto = doctorMapper.mapToDoctorDto(doctor);
-        model.addAttribute("doctor",doctorDto);
-
+        model.addAttribute("doctor", doctorDto);
+        System.out.println(doctorDto.getDateOfBirth());
         return DOCTOR_PROFILE_FILE;
+    }
+
+    @PostMapping(DOCTOR_DOCTOR_PROFILE_URL)
+    public String doctorDoctorProfileUpdate(@Valid @ModelAttribute("user") DoctorDto doctorDto, Model model) {
+        Doctor doctor = doctorMapper.mapToDoctor(doctorDto);
+        doctorService.updateDoctor(doctor);
+        return "redirect:" + DOCTOR_DOCTOR_PROFILE_URL;
     }
 
     @GetMapping(DOCTOR_MESSAGES_URL)
