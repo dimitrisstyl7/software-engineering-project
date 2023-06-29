76405380c342bd3291bd5befec055d302d901fde
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-06-29 20:38:01 +0300

    AdminService.java updated.

--- findoctor/src/main/java/com/unipi/findoctor/services/AdminService.java
+++ findoctor/src/main/java/com/unipi/findoctor/services/AdminService.java
@@ -2,8 +2,6 @@ package com.unipi.findoctor.services;
 
 import com.unipi.findoctor.dto.DoctorDto;
 import com.unipi.findoctor.dto.PatientDto;
-import com.unipi.findoctor.models.Patient;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +9,7 @@ import java.util.List;
 @Service
 public interface AdminService {
     public List<DoctorDto> findAllDoctors();
+
     public List<PatientDto> findAllPatients();
 
     DoctorDto findDoctorByAfm(String Afm);
