a73c7abb4d7978fffc00bd6d38b637f34fe9792a
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-06-29 14:38:15 +0300

    SecurityUtil.java updated.

--- findoctor/src/main/java/com/unipi/findoctor/security/SecurityUtil.java
+++ findoctor/src/main/java/com/unipi/findoctor/security/SecurityUtil.java
@@ -1,7 +1,7 @@
 package com.unipi.findoctor.security;
 
+import com.unipi.findoctor.dto.AuthDto;
 import com.unipi.findoctor.dto.PatientDto;
-import com.unipi.findoctor.dto.UserDto;
 import com.unipi.findoctor.services.PatientService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -9,15 +9,17 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import static com.unipi.findoctor.constants.ControllerConstants.*;
+
 @Service
 @AllArgsConstructor
 public class SecurityUtil {
     private PatientService patientService;
 
-    public UserDto getSessionUser() {
+    public AuthDto getSessionUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
-            return new UserDto(
+            return new AuthDto(
                     authentication.getName(),
                     authentication.getAuthorities().toArray()[0].toString()
             );
@@ -44,11 +46,23 @@ public class SecurityUtil {
     public boolean isPatientLoggedIn() {
         PatientDto patientDto = getSessionPatient();
 
-        if (patientDto == null) {
-            return false;
-        }
-        return true;
-
+        return patientDto != null;
     }
 
+    public String redirectBasedOnUserRole() {
+        // Redirect user based on his role
+        String role = getSessionUser().getUserType();
+
+        switch (role) {
+            case "admin" -> {
+                return "redirect:" + ADMIN_ROOT_URL;
+            }
+            case "doctor" -> {
+                return "redirect:" + DOCTOR_ROOT_URL;
+            }
+            default -> {  // patient
+                return "redirect:" + PATIENT_ROOT_URL;
+            }
+        }
+    }
 }
